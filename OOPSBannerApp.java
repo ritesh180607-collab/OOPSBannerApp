91cc0036d8a12d25be27b121f914c38fbfbd379e
ritesh180607-collab <ritesh180607@gmail.com>
2026-03-01 20:45:19 +0530

    UC5

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,16 +3,17 @@ public class OOPSBannerApp {
     public static void main(String[] args) {
 
         String[] banner = {
-            "  *****     *****     ******     *****  ",
-            " **   **   **   **    **   **    **   ** ",
-            "**     **  **     **   **   **    **      ",
-            "**     **  **     **   ******      *****  ",
-            "**     **  **     **   **              ** ",
-            " **   **   **   **    **         **   **  ",
-            "  *****     *****     **          *****   "
+            String.join("   ", "  ***  ", "  ***  ", " ****  ", "  **** "),
+            String.join("   ", " *   * ", " *   * ", " *   * ", " *     "),
+            String.join("   ", " *   * ", " *   * ", " *   * ", " *     "),
+            String.join("   ", " *   * ", " *   * ", " ****  ", "  ***  "),
+            String.join("   ", " *   * ", " *   * ", " *     ", "     * "),
+            String.join("   ", " *   * ", " *   * ", " *     ", "     * "),
+            String.join("   ", "  ***  ", "  ***  ", " *     ", " ****  ")
         };
-        for (int i = 0; i < banner.length; i++) {
-            System.out.println(banner[i]);
+
+        for (String line : banner) {
+            System.out.println(line);
         }
     }
 }
