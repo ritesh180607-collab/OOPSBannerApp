195c36c328bab0649ef4c2c741e33694ae22a85f
ritesh180607-collab <ritesh180607@gmail.com>
2026-03-01 20:51:21 +0530

    UC6

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,18 +2,50 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join("   ", "  ***  ", "  ***  ", " ****  ", "  **** "),
-            String.join("   ", " *   * ", " *   * ", " *   * ", " *     "),
-            String.join("   ", " *   * ", " *   * ", " *   * ", " *     "),
-            String.join("   ", " *   * ", " *   * ", " ****  ", "  ***  "),
-            String.join("   ", " *   * ", " *   * ", " *     ", "     * "),
-            String.join("   ", " *   * ", " *   * ", " *     ", "     * "),
-            String.join("   ", "  ***  ", "  ***  ", " *     ", " ****  ")
-        };
+        String[] O = generateO();
+        String[] P = generateP();
+        String[] S = generateS();
 
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("   ", O[i], O[i], P[i], S[i]);
+        }
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+    public static String[] generateO() {
+        return new String[]{
+                "  ***  ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                "  ***  "
+        };
+    }
+    public static String[] generateP() {
+        return new String[]{
+                " ****  ",
+                " *   * ",
+                " *   * ",
+                " ****  ",
+                " *     ",
+                " *     ",
+                " *     "
+        };
+    }
+    public static String[] generateS() {
+        return new String[]{
+                "  **** ",
+                " *     ",
+                " *     ",
+                "  ***  ",
+                "     * ",
+                "     * ",
+                " ****  "
+        };
     }
 }
